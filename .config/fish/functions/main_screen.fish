3bb809ca1c57e3f6c062ff7327c12ad5a54c383f
Antonin Godard <antoningodard@pm.me>
2019-05-14 23:41:00 +0200

    Add mode to switch monitors layout
    Switch between different modes with a shortcut
    Remove fish functions to change the layout

--- .config/fish/functions/main_screen.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function main_screen --description 'alias main_screen=xrandr --output eDP1 --primary --mode 1920x1080 --output HDMI1 --off'
-	xrandr --output eDP1 --primary --mode 1920x1080 --output HDMI1 --off $argv;
-end
