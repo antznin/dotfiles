2d3ca0329ee7e8291102aabf75823e100ed5572b
antznin <antonin.godard@telecom-paristech.fr>
2019-10-09 11:49:42 -0400

    Add minor changes

--- .config/i3/screenlayout.sh
+++ .config/i3/screenlayout.sh
@@ -4,5 +4,5 @@ num=$(xrandr --listactivemonitors)
 num=${num:10:1}
 
 if [ $num == 2 ]; then
-xrandr --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x520 --rotate normal --output VGA1 --off
+	xrandr --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off --output eDP1 --primary --mode 1280x1024 --pos 0x520 --rotate normal --output VGA1 --off
 fi
