0786305f15279976b09a86a8a2db68be7265a1e4
antznin <antonin.godard@telecom-paristech.fr>
2020-03-20 17:35:19 +0100

    Change from us to gb

--- .config/i3/toggle_kblayout.sh
+++ .config/i3/toggle_kblayout.sh
@@ -4,7 +4,7 @@
 kblayout=$(setxkbmap -query | grep variant | awk '{print $2}')
 
 if [ "$kblayout" == "intl" ]; then
-	setxkbmap us # set layout on us
+	setxkbmap gb # set layout on us
 else
-	setxkbmap us -variant intl # set on us intl
+	setxkbmap gb -variant intl # set on us intl
 fi
