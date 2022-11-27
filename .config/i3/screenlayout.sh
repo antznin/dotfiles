1c2aa7f814d409300a59e469327eb06727a13858
Antonin Godard <antoningodard@pm.me>
2022-11-27 15:48:59 +0100

    i3: add survival config
    Add a basic config with minimal configurations and what I use most.

--- .config/i3/screenlayout.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-num=$(xrandr --listactivemonitors)
-num=${num:10:1}
-
-if [ $num == 2 ]; then
-	xrandr --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off --output eDP1 --primary --mode 1280x1024 --pos 0x520 --rotate normal --output VGA1 --off
-fi
--- .config/i3/ssh-agent.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [ -z "$SSH_AUTH_SOCK" ] ; then
-	eval $(ssh-agent)
-	ssh-add $HOME/.ssh/id_rsa
-fi
