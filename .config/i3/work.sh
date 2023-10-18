3f8fd112d436b2e2264624c25b6cae2f93463aed
Antonin Godard <antoningodard@pm.me>
2023-10-18 09:25:06 -0700

    Update i3 config
    Major update:
    
    * migration from regolith
    * white theme
    * add some useful scripts

---
+++ .config/i3/work.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+day="$(date +%u)"
+hour="$(date +%H)"
+
+if [[ ! $(date +%u) -gt 5 ]]; then
+    if [ $hour -ge 8 ] && [ $hour -lt 18 ]; then
+        slack & disown
+        teams & disown
+    fi
+fi
