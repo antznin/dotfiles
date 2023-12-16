c4521ca498c8f0330c1f791356ddd4bd59525a0e
Antonin Godard <antoningodard@pm.me>
2023-12-15 18:12:09 -0800

    Update i3 config

--- .config/i3/wezterm-cwd.sh
+++ .config/i3/wezterm-cwd.sh
@@ -1,41 +1,43 @@
 #!/bin/bash
-# Spawn a new instance of Alacritty using the CWD of the currently focused
-# Alacritty process.
+# Spawn a new instance of Wezterm using the CWD of the currently focused
+# Wezterm process.
 #
 # This is useful in environment like i3 where terminals are opened using a
 # key-combination while another terminal is already focused.
 #
 # If the script is run with a non-Alacritty window in focus or a non-compliant
-# version of Alacritty, an instance will be spawned in the user's $HOME.
+# version of Wezterm, an instance will be spawned in the user's $HOME.
 
-# Credit: https://github.com/alacritty/alacritty/issues/808#issuecomment-334200570.
+# Credit: https://github.com/Wezterm/Wezterm/issues/808#issuecomment-334200570.
 
-# Prevent alacritty from scaling.
+# Prevent Wezterm from scaling.
 export WINIT_X11_SCALE_FACTOR=1
 
 ACTIVE_WINDOW=$(xdotool getactivewindow)
 ACTIVE_WM_CLASS=$(xprop -id $ACTIVE_WINDOW | grep WM_CLASS)
 
-if [[ $ACTIVE_WM_CLASS == *"Alacritty"* ]]
+echo ""
+
+if [[ $ACTIVE_WM_CLASS == *"wezterm"* ]]
 then
     # Get PID. If _NET_WM_PID isn't set, bail.
     PID=$(xprop -id $ACTIVE_WINDOW | grep _NET_WM_PID | grep -oP "\d+")
     if [[ "$PID" == "" ]]
     then
-        alacritty $@ &
+        wezterm start --always-new-process $@ &
     fi
     # Get first child of terminal
     CHILD_PID=$(pgrep -P $PID)
     if [[ "$PID" == "" ]]
     then
-        alacritty $@ &
+        wezterm start --always-new-process $@ &
     fi
     # Get current directory of child. The first child should be the shell.
     pushd "/proc/${CHILD_PID}/cwd"
     SHELL_CWD=$(pwd -P)
     popd
-    # Start alacritty with the working directory
-    alacritty --working-directory $SHELL_CWD $@ &
+    # Start wezterm with the working directory
+    wezterm start --always-new-process --cwd $SHELL_CWD $@ &
 else
-    alacritty $@ &
+    wezterm start --always-new-process $@ &
 fi
--- .config/i3/work.sh
+++ .config/i3/work.sh
@@ -7,5 +7,6 @@ if [[ ! $(date +%u) -gt 5 ]]; then
     if [ $hour -ge 8 ] && [ $hour -lt 18 ]; then
         slack & disown
         teams & disown
+        thunderbird & disown
     fi
 fi
