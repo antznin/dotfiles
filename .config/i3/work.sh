0de820d426dd0531742c3ad1457db230cf3c0ee3
antznin <antoningodard@pm.me>
2024-04-20 17:51:58 -0700

    Update i3 config
    Signed-off-by: antznin <antoningodard@pm.me>

--- .config/i3/work.sh
+++ .config/i3/work.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-day="$(date +%u)"
 hour="$(date +%H)"
 
+declare -a apps=(
+    slack
+    thunderbird
+    protonmail-bridge
+    teams
+)
+
 if [[ ! $(date +%u) -gt 5 ]]; then
-    if [ $hour -ge 8 ] && [ $hour -lt 18 ]; then
-        slack & disown
-        teams & disown
-        thunderbird & disown
+    if [ "$hour" -ge "8" ] && [ "$hour" -lt "18" ]; then
+        for app in "${apps[@]}"; do
+            $app & disown
+        done
     fi
 fi
